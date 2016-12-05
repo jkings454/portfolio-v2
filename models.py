2dfbb3054ee36498772f88d3e426a9e0a060f8a3
Josh Nichols <josh.nichols.lives@gmail.com>
2016-12-05 16:58:40 -0700

    Tried to start on models

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
-from sqlalchemy import Column, String, Integer, ForeignKey, Relationship
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import create_engine
+from passlib import pwd
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "user"
