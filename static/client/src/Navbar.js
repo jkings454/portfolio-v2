38102ed80d69669d50f895d1b55c3dddbd3882b3
Josh Nichols <josh.nichols.lives@gmail.com>
2016-12-24 12:43:25 -0700

    Practicing my ReactJs skillz

--- static/client/src/Navbar.js
+++ static/client/src/Navbar.js
@@ -2,11 +2,30 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Navbar = React.createClass({
-    render: function() {
+    render: function () {
+        var clickFunction = this.props.onClick;
         return (
-            <nav className = 'navbar navbar-default navbar-static-top'>
+            <nav className='navbar navbar-default navbar-static-top'>
                 <div className='container'>
-                    <h1>Hello world!</h1>
+                    <div className='navbar-header'>
+                        <a href='#' className='navbar-brand'
+                            onClick={() => (clickFunction('index'))}>Josh Nichols</a>
+                        <button type='button' className='navbar-toggle'
+                            data-toggle='collapse' data-target='#my-collapse'>
+                            <span className='icon-bar'></span>
+                            <span className='icon-bar'></span>
+                            <span className='icon-bar'></span>
+                        </button>
+                    </div>
+                    <div className='collapse navbar-collapse' id='my-collapse'>
+                        <ul className='nav navbar-nav navbar-right'>
+                            <li><a href="#" key='index' onClick={() => (clickFunction("index"))}>Home</a></li>
+                            {this.props.pages.map(function (page) {
+                                return (<li><a href="#" key={page}
+                                    onClick={() => (clickFunction(page))}>{page}</a></li>);
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </nav>);
     },
--- static/client/src/index.js
+++ static/client/src/index.js
@@ -1,11 +1,35 @@
-var React = require("react")
-var ReactDOM = require("react-dom")
-var Navbar = require("./Navbar")
+const React = require("react");
+const ReactDOM = require("react-dom");
+const Navbar = require("./Navbar");
 
-var App = React.createClass({
+const pages = {
+    "index":(<div className = 'container'>
+                <h1>This is the index page!</h1>
+            </div>),
+    "About":(<div className = 'container'>
+                <h1>This is the about page!</h1>
+            </div>),
+    "Projects":(<div className = 'container'>
+                <h1>This is the projects page!</h1>
+            </div>),
+};
+const App = React.createClass({
+    getInitialState: function() {
+        return { currentPage: "index" }
+    },
+    onNavLinkClicked: function(page){
+        this.setState(
+           { currentPage: page }
+       );
+    },
     render: function() {
-        return (<Navbar/>);
+        return (<div>
+                    <Navbar pages = {['About', 'Projects']} onClick = {this.onNavLinkClicked} />
+                    {pages[this.state.currentPage]}
+                </div>);
     }
 });
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <App/>, 
+    document.getElementById('app'));
